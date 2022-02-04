b1432813973f01e48295fb802f6ecd8d477c2226
mflanigan <mflanigantwualumn@gmail.com>
2022-02-04 11:29:43 -0600

    outlined functions for app and wrote function show departments, roles, and employees

--- server.js
+++ server.js
@@ -110,3 +110,70 @@ const promptUser = () => {
             };
         });
 };
+
+// function to show all departments 
+showDepartments = () => {
+    console.log('Showing all departments...\n');
+    const sql = `SELECT department.id AS id, department.name AS department FROM department`;
+
+    connection.promise().query(sql, (err, rows) => {
+        if (err) throw err;
+        console.table(rows);
+        promptUser();
+    });
+};
+
+// function to show all roles 
+showRoles = () => {
+    console.log('Showing all roles...\n');
+
+    const sql = `SELECT role.id, role.title, department.name AS department
+                    FROM role
+                    INNER JOIN department ON role.department_id = department.id`;
+
+    connection.promise().query(sql, (err, rows) => {
+        if (err) throw err;
+        console.table(rows);
+        promptUser();
+    })
+};
+
+// function to show all employees 
+showEmployees = () => {
+    console.log('Showing all employees...\n');
+    const sql = `SELECT employee.id, 
+                        employee.first_name, 
+                        employee.last_name, 
+                        role.title, 
+                        department.name AS department,
+                        role.salary, 
+                        CONCAT (manager.first_name, " ", manager.last_name) AS manager
+                    FROM employee
+                        LEFT JOIN role ON employee.role_id = role.id
+                        LEFT JOIN department ON role.department_id = department.id
+                        LEFT JOIN employee manager ON employee.manager_id = manager.id`;
+
+    connection.promise().query(sql, (err, rows) => {
+        if (err) throw err;
+        console.table(rows);
+        promptUser();
+    });
+};
+
+// function to add a department 
+
+// function to add a role 
+
+// function to add an employee 
+
+// function to update an employee 
+
+// function to view employee by department
+
+// function to delete department
+
+// function to delete role
+
+// function to delete employees
+
+// view department budget 
