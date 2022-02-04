e81e8b76227e6ff5463dec22eab34613fc883809
mflanigan <mflanigantwualumn@gmail.com>
2022-02-03 21:02:35 -0600

    started writing seeds file

---
+++ db/seeds.sql
@@ -0,0 +1,16 @@
+INSERT INTO department (name)
+VALUES 
+('IT'),
+('Finance & Accounting'),
+('Sales & Marketing'),
+('Operations');
+
+INSERT INTO role (title, salary, department_id)
+VALUES
+('Full Stack Developer', 80000, 1),
+
+
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES 
+('Mark', 'Miller', 2, null),
